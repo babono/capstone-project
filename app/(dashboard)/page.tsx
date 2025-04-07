63e4a34a981f6fae6e0995a59b0dfe6d2698d730
babono <babono@hotmail.com>
2025-04-07 14:21:13 +0800

    fix bug

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { useSession } from "next-auth/react"
 import { useRouter } from "next/navigation"
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 import Image from "next/image"
 import iconDT from "../../public/ic-dt.svg"
 
@@ -9,27 +9,12 @@ export default function HomePage() {
   const { data: session, status } = useSession()
   const router = useRouter()
 
-  const [prompt, setPrompt] = useState('')
-  const [insight, setInsight] = useState('')
 
   useEffect(() => {
     if (status === "loading") return // Do nothing while loading
     if (!session) router.push("/login")
   }, [session, status, router])
 
-
-  const handlePromptSubmit = async () => {
-    const response = await fetch('/api/insight', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ prompt }),
-    })
-    console.log(response)
-    const data = await response.json()
-    setInsight(data.response)
-  }
 
   if (status === "loading" || !session) {
     return (
